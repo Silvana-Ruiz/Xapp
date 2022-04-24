409b410f49c5ba84619efbe770857d81dcfa7936
Alam López Montoya <A01252135@tec.mx>
2022-04-23 22:04:16 -0700

    login "terminado"

---
+++ Xapp/wwwroot/XAPP_js/login_code.js
@@ -0,0 +1,21 @@
+﻿// PopUp de Skills
+var closeBut = document.getElementsByClassName('close')[0],
+    modal = document.getElementsByClassName('modal-cont')[0],
+    loginBut = document.getElementsByClassName('contra-button')[0];
+
+//close
+function x() {
+    modal.style.display = "none";
+}
+closeBut.onclick = x;
+
+loginBut.onclick = function () {
+    modal.style.display = "block";
+}
+
+window.onclick = function (e) {
+    if (e.target.className === 'modal-cont') {
+        e.target.style.display = 'inline-block';
+    }
+}
+
